8132090b953c3a57926bafc5b1e2736eaba7cc47
Edith Chow <edith.chow@arup.com>
2020-04-30 22:03:29 -0400

    added cleaning of xml.

--- performdiff.js
+++
@@ -1,9 +0,0 @@
-function getdiff() {
-    document.getElementById('status').content = 'Button pressed.'
-    let originalText = document.getElementById('original').message
-    let newText = document.getElementById('new').message
-
-    let output = htmldiff(originalText, newText);
-
-    document.getElementById('output').innerHTML = output
-}
